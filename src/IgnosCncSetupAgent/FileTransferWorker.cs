ba438d792a6620bd11ac53eb565ce9325f50fc48
Tom Thorsen <tom.thorsen@ignos.no>
2024-01-25 14:17:05 +0100

    [Keep] Agent termination handling
    (brutal) exit with exit code on exception to ensure service manager notices
    Flush Application Insights telemetry before any exit

--- src/IgnosCncSetupAgent/FileTransferWorker.cs
+++ src/IgnosCncSetupAgent/FileTransferWorker.cs
@@ -1,12 +1,14 @@
 using IgnosCncSetupAgent.Config;
 using IgnosCncSetupAgent.FileTransfer;
 using IgnosCncSetupAgent.Messaging;
+using Microsoft.ApplicationInsights;
 
 namespace IgnosCncSetupAgent;
 
 public class FileTransferWorker(IAgentConfigService agentConfigService,
     IServiceBusListenerFactory serviceBusListenerFactory,
     IFileTransferHandlers fileTransferHandlers,
+    TelemetryClient telemetryClient,
     ILogger<FileTransferWorker> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -38,10 +40,31 @@ public class FileTransferWorker(IAgentConfigService agentConfigService,
                 serviceBusListener.Reconfigure(config);
             }
         }
+        catch (OperationCanceledException)
+        {
+            // When the stopping token is canceled, for example, a call made from services.msc,
+            // we shouldn't exit with a non-zero exit code. In other words, this is expected...
+        }
         catch (Exception ex)
         {
             logger.LogError(ex, $"Exception in {nameof(FileTransferWorker)}");
-            throw;
+
+            //https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service
+            // Terminates this process and returns an exit code to the operating system.
+            // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
+            // performs one of two scenarios:
+            // 1. When set to "Ignore": will do nothing at all, errors cause zombie services.
+            // 2. When set to "StopHost": will cleanly stop the host, and log errors.
+            //
+            // In order for the Windows Service Management system to leverage configured
+            // recovery options, we need to terminate the process with a non-zero exit code.
+
+            Environment.Exit(1);
+        }
+        finally
+        {
+            //https://learn.microsoft.com/en-us/azure/azure-monitor/app/api-custom-events-metrics#flushing-data
+            await telemetryClient.FlushAsync(CancellationToken.None);
         }
     }
 }
